3039efb2f2f95b282d2331a579327f6b743a25b5
Cyril Verluise <cyril.verluise@gmail.com>
2020-10-12 23:24:29 +0200

    :package: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="patentcity",
+    version="0.0",
+    packages=["patentcity"],
+    url="https://github.com/Antoberge/patentcity",
+    license="MIT",
+    author="Cyril Verluise & Antonin Bergeaud",
+    author_email="cyril.verluise@gmail.com",
+    description="Where Innovation happens",
+)
