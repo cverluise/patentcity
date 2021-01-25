f0c2fe829da16aa262bbc62c1ce69dc3a4b830d0
Margaux <margaux@MacBookeMargaux.lan>
2021-01-25 12:13:43 +0100

    :tada: add search and eval

--- patentcity/main.py
+++ patentcity/main.py
@@ -3,6 +3,8 @@ from patentcity import geo
 from patentcity import brew
 from patentcity import utils
 from patentcity import io
+from patentcity import eval
+from patentcity import search
 
 app = typer.Typer()
 
@@ -10,7 +12,8 @@ app.add_typer(geo.app, name="geo")
 app.add_typer(brew.app, name="brew")
 app.add_typer(io.app, name="io")
 app.add_typer(utils.app, name="utils")
-
+app.add_typer(eval.app, name="eval")
+app.add_typer(search.app, name="search")
 
 if __name__ == "__main__":
     app()
