d0b7bee7733656fa99fb1b65de0e7787cdac3740
Margaux <margaux@MacBookeMargaux.lan>
2021-01-28 18:12:34 +0100

    :tada: add draft of mcq.geoc

--- prodigy/mcq_recipe.py
+++ prodigy/mcq_recipe.py
@@ -3,12 +3,12 @@ from prodigy.components.loaders import JSONL
 
 
 @prodigy.recipe(
-    "mcq",
+    "mcq.loc",
     dataset=("The dataset to save to", "positional", None, str),
     file_path=("Path to texts", "positional", None, str),
 )
 def mcq(dataset, file_path):
-    """Validate the disambiguated LOC of raw locs extracted from ."""
+    """Validate the disambiguated LOC of raw locs extracted from the patent texts."""
 
     def add_html(stream):
         for task in stream:
@@ -40,4 +40,38 @@ def mcq(dataset, file_path):
                 # {"view_id": "html"}
             ]
         },
+    }
+
+
+@prodigy.recipe(
+    "mcq.geoc",
+    dataset=("The dataset to save to", "positional", None, str),
+    file_path=("Path to texts", "positional", None, str),
+)
+def mcq(dataset, file_path):
+    """Validate the geocoded LOC of raw locs extracted from the patent texts."""
+
+    def add_options(stream):
+        # Helper function to add options to every task in a stream
+        options = [
+            {"id": "country", "text": "country"},
+            {"id": "state", "text": "state"},
+            {"id": "county", "text": "county"},
+            {"id": "city", "text": "city"},
+            {"id": "district", "text": "district"},
+            {"id": "street", "text": "street"},
+            {"id": "house", "text": "house"},
+        ]
+        for task in stream:
+            task["options"] = options
+            task["text"] = "\n".join([task["loc_text"], task["loc_locationLabel"]])
+            yield task
+
+    stream = JSONL(file_path)  # load in the JSONL file
+    stream = add_options(stream)  # add options to each task
+
+    return {
+        "dataset": dataset,  # save annotations in this dataset
+        "view_id": "choice",  # use the choice interface
+        "stream": stream,
     }
