d04f8a8a163331a95ab01d8596dd1df6a94be07e
Cyril Verluise <cyril.verluise@gmail.com>
2021-02-14 23:42:53 +0100

    :bug: fix major issue with legacy record tracking

--- patentcity/brew.py
+++ patentcity/brew.py
@@ -1,7 +1,7 @@
 import json
 import yaml
 import os
-from glob import iglob
+from glob import iglob, glob
 from smart_open import open
 import git
 from concurrent.futures import ThreadPoolExecutor
@@ -10,6 +10,7 @@ from itertools import repeat
 from patentcity.relationship import create_relationship_component
 import spacy
 import typer
+from hashlib import md5
 
 from patentcity.utils import clean_text, get_recid, get_cit_code
 
@@ -28,8 +29,7 @@ sha = repo.head.object.hexsha
 def get_text(file, max_char=None):
     with open(file, "r") as fin:
         text = fin.read()[:max_char] if max_char else fin.read()
-        publication_number = os.path.splitext(os.path.basename(file))[0]
-        return " ".join([publication_number, text])
+        return text
 
 
 @app.command(deprecated=True)
@@ -102,38 +102,41 @@ def v1(
     path: str,
     model: str,
     rel_config: str,
-    max_char: int = None,
+    max_char: int = 9999,
     batch_size: int = 1000,
-    reduced_perf: bool = False,
     inDelim: str = "|",
 ):
     """
     Print jsonl blobs of the v1 dataset
     """
+    nlp = spacy.load(model)
     with open(rel_config, "r") as config_file:
         config = yaml.load(config_file, Loader=yaml.FullLoader)
-
-    nlp = spacy.load(model)
     nlp.add_pipe("relation_extractor", config={"config": config}, last=True)
-    blobs = iglob(path)
-    texts = (get_text(file, max_char=max_char) for file in blobs)
-    if reduced_perf:
-        docs = nlp.pipe(texts, n_threads=1, batch_size=1)
-    else:
-        docs = nlp.pipe(texts, batch_size=batch_size)
-    for doc in docs:
-        patentees = [
-            {k: clean_text(v, inDelim) for k, v in patentee.items()}
-            for patentee in doc._.patentees
-        ]
-        row = {
-            "publication_number": doc[0].text,
-            "patentee": patentees,
-            "model_ents": model,
-            "model_rels": rel_config,
-            "git_sha": sha,
-        }
-        typer.echo(json.dumps(row))
+
+    files = glob(path)
+    for file in files:
+        publication_numbers = list((json.loads(line)["publication_number"] for line in open(file, "r")))
+        hash_ids = list((json.loads(line)["hash_id"] for line in open(file, "r")))
+        with open(file, "r") as lines:
+            texts = (json.loads(line)["text"][:max_char] for line in lines)
+            docs = nlp.pipe(texts, batch_size=batch_size)
+            for i, doc in enumerate(docs):
+                publication_number = publication_numbers[i]
+                hash_id = hash_ids[i]
+                patentees = [
+                    {k: clean_text(v, inDelim) for k, v in patentee.items()}
+                    for patentee in doc._.patentees
+                ]
+                row = {
+                    "publication_number": publication_number,
+                    "patentee": patentees,
+                    "hash_id": hash_id,
+                    "model_ents": model,
+                    "model_rels": rel_config,
+                    "git_sha": sha,
+                }
+                typer.echo(json.dumps(row))
 
 
 def topping_(line, cit_fst):
