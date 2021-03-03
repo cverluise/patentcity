f0209718f235b96f4236254f55b76f59d3544f17
Cyril Verluise <cyril.verluise@gmail.com>
2021-03-03 22:12:18 +0100

    :tada: add US county GMAPS->HERE harmonization

--- patentcity/geo.py
+++ patentcity/geo.py
@@ -20,6 +20,7 @@ from patentcity.lib import (
     HERE2GMAPS,
     get_isocrossover,
     get_usstatecrossover,
+    get_uscountycrossover,
     TYPE2LEVEL,
 )
 from patentcity.utils import clean_text, get_dt_human, get_empty_here_schema, flatten
@@ -462,7 +463,7 @@ def emulate_nomatch_gmaps(recid):
     return out
 
 
-def parse_response_gmaps(response, recid, out_format, iso_crossover, us_state_crossover):
+def parse_response_gmaps(response, recid, out_format, iso_crossover, us_state_crossover, us_county_crossover):
     """Parse the high level Gmaps response (list of results). Can contain more than 1 results as
     well as 0."""
 
@@ -487,6 +488,18 @@ def parse_response_gmaps(response, recid, out_format, iso_crossover, us_state_cr
             out.update({"country": iso_crossover.get(out.get("country"))})
             # US state to code form
             out.update({"state": us_state_crossover.get(out.get("state"), out.get("state"))})
+            # US county 'harmonization'
+            if out.get("country") == "USA" and out.get("county"):
+                county_ = (out.get("county")
+                           .replace("County", "")
+                           .replace("Parish", "")
+                           .replace("Borough", "")
+                           .replace("Census Area", "")
+                           .replace("St.", "St")
+                           .replace("Ste.", "Ste")
+                           .replace("'s", "s").strip())
+                county_ = us_county_crossover.get(county_, county_)
+                out.update({"county": county_})
             # matchLevel harmonization
             out.update({"matchLevel": types2level_crossover(out["matchLevel"])})
             if not out.get("latitude"):
@@ -506,6 +519,7 @@ def harmonize_geoc_data_gmaps(
     assert out_format in ["csv", "jsonl"]
     iso_crossover = get_isocrossover()
     us_state_crossover = get_usstatecrossover()
+    us_county_crossover = get_uscountycrossover()
 
     if out_format == "csv" and header:
         csvwriter = csv.DictWriter(sys.stdout, GEOC_OUTCOLS)
@@ -518,7 +532,8 @@ def harmonize_geoc_data_gmaps(
 
             try:
                 recid, response = line.split(inDelim)
-                parse_response_gmaps(response, recid, out_format, iso_crossover, us_state_crossover)
+                parse_response_gmaps(response, recid, out_format,
+                                     iso_crossover, us_state_crossover, us_county_crossover)
             except ValueError:
                 pass
                 # occurs when there is still an inDelim in the result
